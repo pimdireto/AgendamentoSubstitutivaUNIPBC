8ca5c89ecf79c0ae50861b1ff2381c985e3deecc
pimdireto <pimdireto@gmail.com>
2025-11-13 19:03:40 -0300

    3 teste
    Tetstando funcionar a planilha

--- assets/js/script.js
+++ assets/js/script.js
@@ -93,7 +93,7 @@ function enviarFormulario() {
     }
   }, 100);
 
-  const URL_API = "https://script.google.com/macros/s/AKfycbxdSxRCxSc-3X8xycA-Ss_Ve0pCq3G3CWzUQEhooH_UoTpiLAfCTB6M9bsXLIxj_o95/exec";
+  const URL_API = "https://script.google.com/macros/s/AKfycbyksH_GkQABIE8sR3MfgkYHo2QDpEdYeb1XyBigXaJPpmrdgUJetAh9RuUqABeszYFL/exec";
 
   fetch(`${URL_API}?ra=${ra}`)
     .then(res => res.json())
@@ -188,7 +188,7 @@ function consultarAgendamento() {
   }
 
   document.getElementById("loadingOverlay").style.display = "flex";
-  const URL_API = "https://script.google.com/macros/s/AKfycbxdSxRCxSc-3X8xycA-Ss_Ve0pCq3G3CWzUQEhooH_UoTpiLAfCTB6M9bsXLIxj_o95/exec";
+  const URL_API = "https://script.google.com/macros/s/AKfycbyksH_GkQABIE8sR3MfgkYHo2QDpEdYeb1XyBigXaJPpmrdgUJetAh9RuUqABeszYFL/exec";
 
   fetch(`${URL_API}?ra=${ra}`)
     .then(res => res.json())
@@ -246,7 +246,7 @@ let horariosLotados = {};
 
 async function carregarHorariosLotados() {
   try {
-    const response = await fetch("https://script.google.com/macros/s/AKfycbxdSxRCxSc-3X8xycA-Ss_Ve0pCq3G3CWzUQEhooH_UoTpiLAfCTB6M9bsXLIxj_o95/exec");
+    const response = await fetch("https://script.google.com/macros/s/AKfycbyksH_GkQABIE8sR3MfgkYHo2QDpEdYeb1XyBigXaJPpmrdgUJetAh9RuUqABeszYFL/exec");
     const data = await response.json();
     horariosLotados = data;
   } catch (error) {
@@ -258,6 +258,7 @@ document.addEventListener("DOMContentLoaded", async () => {
   await carregarHorariosLotados();
   // Se houver função para criar grade, chame aqui
 });
+
 
 
 
